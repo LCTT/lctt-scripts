11f99980352706a7ac81a2993622b456b9d786c3
darksun <lujun9972@gmail.com>
2017-12-03 11:13:17 +0800

    update

--- 1_add_new_article.sh
+++ 1_add_new_article.sh
@@ -51,7 +51,7 @@ sed -i "/-------------------------------/,$ s^via: 网址^via: ${url}^" "${sourc
 sed -i "/-------------------------------/,$ s^\[a\]:$^[a]:${baseurl}^" "${source_file}"
 
 # 新建branch 并推送新文章
-new_branch="add-$(title-to-branch '${title}')"
+new_branch="add-$(title-to-branch "${title}")"
 git branch "${new_branch}" master
 git checkout "${new_branch}"
 git add "${source_file}"

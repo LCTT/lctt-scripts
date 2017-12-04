91807143856d272291b7d1819d5660a38c148bb1
darksun <lujun9972@gmail.com>
2017-12-04 09:35:27 +0800

    -d option means delete the branch

--- 4_finish.sh
+++ 4_finish.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 source base.sh
+while getopts :d OPT; do
+    case $OPT in
+        d|+d)
+            delete_branch=1
+            ;;
+        *)
+            echo "usage: ${0##*/} [+-d}"
+            exit 2
+    esac
+done
+shift $(( OPTIND - 1 ))
+OPTIND=1
 
 cd $(get-lctt-path)
 git add .
 git commit -m "update at $(date)"
 git push
-
+current_branch=$(git-get-current-branch)
 git checkout master
+if [[ -n "${delete_branch}" ]];then
+    git branch -d "${current_branch}"
+fi

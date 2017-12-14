c8fb8be597a4203c6fa403bef6cd9790c79c182e
darksun <lujun9972@gmail.com>
2017-12-14 18:36:03 +0800

    两种添加方式都提供

---
+++ 1_add_new_article_manual.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+set -e
+source base.sh
+
+# 获取参数
+while getopts :u:t:d:T OPT; do
+    case $OPT in
+        u|+u)
+            url="$OPTARG"
+            ;;
+        t|+t)
+            title="$OPTARG"
+            ;;
+        d|+d)
+            date="$OPTARG"
+            ;;
+        T|+T)
+            tranlate_flag="yes"
+            ;;
+        *)
+            echo "usage: ${0##*/} [+-u url] [+-t title] [+-d date] [+-T]}"
+            exit 2
+    esac
+done
+shift $(( OPTIND - 1 ))
+OPTIND=1
+
+[[ -z "${url}" ]] && read -r -p "please input the URL:" url
+baseurl=$(get-domain-from-url "${url}")
+if url-blocked-p "${baseurl}";then
+    warn "${baseurl} is blocked!"
+    exit 1
+fi
+
+[[ -z "${title}" ]] && read -r -p "please input the Title:" title
+[[ -z "${date}" ]] && read -r -p "please input the date(YYYYMMDD):" date
+
+# 搜索类似的文章
+echo "search simliar articles..."
+if search-similar-articles "$title";then
+    continue-p "found similar articles"
+fi
+
+# 生成新文章
+cd "$(get-lctt-path)"
+source_path="$(get-lctt-path)/sources/tech"
+filename=$(date-title-to-filename "${date}" "${title}")
+source_file="${source_path}"/"${filename}"
+
+$(get-browser) "${url}" "http://lctt.ixiqin.com"
+$(get-editor) "${source_file}"
+
+read -p "保存好原稿了吗？按回车键继续" continue
+sed -i "/-------------------------------/,$ s^via: 网址^via: ${url}^" "${source_file}"
+sed -i "/-------------------------------/,$ s^\[a\]:$^[a]:${baseurl}^" "${source_file}"
+
+if [[ -n ${tranlate_flag} ]];then
+    mark-file-as-tranlating "${source_file}"
+fi
+
+# 新建branch 并推送新文章
+filename=$(basename "${source_file}")
+# new_branch="add-$(title-to-branch "${filename}")"
+new_branch="$(filename-to-branch add "${filename}")"
+git branch "${new_branch}" master
+git checkout "${new_branch}"
+git add "${source_file}"
+git commit -m "选题: ${title}"
+git push -u origin "${new_branch}"

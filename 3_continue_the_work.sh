0f67ba75e3fe8b3406784526fd9b37d0e6b23cbf
darksun <lujun9972@gmail.com>
2017-12-25 09:41:59 +0800

    检查当前branch是否为master
    不能在master branch上操作，若为master分支则列出其他分支供用户checkout

--- 3_continue_the_work.sh
+++ 3_continue_the_work.sh
@@ -3,5 +3,20 @@ set -e
 source base.sh
 
 cd "$(get-lctt-path)"
+current_branch=$(git-get-current-branch)
+if [[ "$current_branch" == "master" ]];then
+    i=0
+    while read branch;do
+        operation=$(git-branch-to-operation "${branch}")
+        filename=$(git-branch-to-filename "${branch}")
+        printf "%3d. %10s %s\n" $i "${operation}" "${filename}"
+        branches[$i]=$branch
+        i=$((i+1))
+    done < <(git branch |grep -v 'master')
+    echo "YOU ARE UNDER BRANCH MASTER"
+    read -r -p "Which branch do you want to checkout(input the id): " num
+    git checkout "${branches[$i]}"
+fi
+
 filepath=$(git-current-branch-to-file-path)
 $(get-editor) "${filepath}"

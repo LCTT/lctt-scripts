4667826125c34fe83eff89c83491348093900a10
darksun <lujun9972@gmail.com>
2018-01-29 12:59:42 +0800

    add strict mode

--- urls_checker.sh
+++ urls_checker.sh
@@ -1,25 +1,56 @@
 #!/bin/bash
-if [[ $# -eq 0 ]];then
+function Usage
+{
     cat <<EOF
 Usage:
-$(basename $0) list sources/published/translated   -- 列出尚未翻译/已翻译/已发布的url
-$(basename $0) comm sources/published/translated  sources/published/translated -- 检查是否有重复的urls
+${0##*/} [+-lcs} [--] ARGS...
+${0##*/} -l sources/published/translated   -- 列出尚未翻译/已翻译/已发布的url
+${0##*/} -c sources/published/translated  sources/published/translated -- 检查是否有重复的urls
+与-s选项连用则表示进入strict mode，这个mode下不会去掉url中?后面的参数
 EOF
-    exit 0
-else
-    operation=$1
-    directory1=$2
-    directory2=$3
+}
+if [[ $# -le 1 ]];then
+    Usage
+    exit 2
 fi
+strict=0
+while getopts :lcs OPT; do
+    case $OPT in
+        l|+l)
+            operation="list"
+            ;;
+        c|+c)
+            operation="comm"
+            ;;
+        s|+s)
+            strict=1
+            ;;
+        *)
+            echo "usage: "
+            exit 2
+    esac
+done
+shift $(( OPTIND - 1 ))
+OPTIND=1
+directory1=$1
+directory2=$2
 
 set -e
 source $(dirname "${BASH_SOURCE[0]}")/base.sh
 cd "$(get-lctt-path)"
 
-function list_urls()
-{
-    git grep -E "^via:.+$" $1 |sed "s/^.\+via:[[:space:]]*//"|sed "s/?.\+$//"|sort
-}
+if [[ ${strict} -eq 0 ]];then
+    function list_urls()
+    {
+        git grep -E "^via:.+$" $1 |sed "s/^.\+via:[[:space:]]*//"|sed "s/?.\+$//"|sort
+    }
+else
+    function list_urls()
+    {
+        git grep -E "^via:.+$" $1 |sed "s/^.\+via:[[:space:]]*//"|sort
+    }
+fi
+
 
 if [[ "${operation}" == "list" ]];then
     list_urls "${directory1}"
@@ -28,4 +59,3 @@ elif [[ "${operation}" == "comm" ]];then
 else
     warn "不支持的操作:${operation}"
 fi
-

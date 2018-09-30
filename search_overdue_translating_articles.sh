2c4a0138ec2ba4dbb28b74fdc9732278b0eb42af
darksun <lujun9972@gmail.com>
2018-09-30 22:28:07 +0800

    增加 `结束超期天数` 的限制

--- search_overdue_translating_articles.sh
+++ search_overdue_translating_articles.sh
@@ -3,14 +3,31 @@ set -e
 source $(dirname "${BASH_SOURCE[0]}")/base.sh
 cd $(get-lctt-path)
 
-timeout=${1:-30}
-timeout=$(($timeout * 24 * 60 * 60))
+function help()
+{
+    cat <<EOF
+Usage: $0 起始超期天数 [结束超期天数]
+
+列出超期未翻译完成的文章，超期日期>=起始超期天数 同时 超期日期<=结束超期天数
+若结束超期天数省略，则列出 超期日期>=起始超期天数 的文章
+EOF
+}
+
+if [[ $# -eq 0 ]];then
+    help
+    exit 1
+fi
+
 now=$(date +"%s")
-overdue=$((${now} - ${timeout}))
+timeout_start=$(($1 * 24 * 60 * 60))
+timeout_end=$((${2:-9999999} * 24 * 60 * 60))
+overdue_start=$((${now} - ${timeout_start}))
+overdue_end=$((${now} - ${timeout_end}))
 git grep -niE "translat|fanyi|翻译"  sources/*.md |sort -t ":" -g -k 2 |grep ":1:"|cut -d : -f1 |while read article
 do
-    last_modify_time=$(git log --date=unix --pretty=format:"%cd" -n 1 "${article}" )
-    if [[ ${last_modify_time} -lt ${overdue} ]];then
-        echo "${article}"
+    translating_time=$(git log --date=unix --pretty=format:"%cd" -n 1 "${article}" )
+    if [[ ${translating_time} -le ${overdue_start} &&  ${translating_time} -ge ${overdue_end} ]];then
+        delay_days=$(( ($now - $translating_time) / 24 / 60 / 60 ))
+        echo "${article}"       # "------" "${delay_days}天"
     fi
 done

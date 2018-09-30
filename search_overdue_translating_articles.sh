4bca7326ec510af6bf5ba83e78a4549e33d4b641
darksun <lujun9972@gmail.com>
2018-09-30 22:31:12 +0800

    规则微调: 超期日期<结束超期天数

--- search_overdue_translating_articles.sh
+++ search_overdue_translating_articles.sh
@@ -8,7 +8,7 @@ function help()
     cat <<EOF
 Usage: $0 起始超期天数 [结束超期天数]
 
-列出超期未翻译完成的文章，超期日期>=起始超期天数 同时 超期日期<=结束超期天数
+列出超期未翻译完成的文章，超期日期>=起始超期天数 同时 超期日期<结束超期天数
 若结束超期天数省略，则列出 超期日期>=起始超期天数 的文章
 EOF
 }
@@ -26,8 +26,8 @@ overdue_end=$((${now} - ${timeout_end}))
 git grep -niE "translat|fanyi|翻译"  sources/*.md |sort -t ":" -g -k 2 |grep ":1:"|cut -d : -f1 |while read article
 do
     translating_time=$(git log --date=unix --pretty=format:"%cd" -n 1 "${article}" )
-    if [[ ${translating_time} -le ${overdue_start} &&  ${translating_time} -ge ${overdue_end} ]];then
+    if [[ ${translating_time} -le ${overdue_start} &&  ${translating_time} -gt ${overdue_end} ]];then
         delay_days=$(( ($now - $translating_time) / 24 / 60 / 60 ))
-        echo "${article}"       # "------" "${delay_days}天"
+        echo "${article}"        "------" "${delay_days}天"
     fi
 done

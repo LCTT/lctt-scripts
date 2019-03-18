3a93bbf90089bc83a227e3d783d80ebb3d289099
darksun <lujun9972@gmail.com>
2019-03-18 18:19:17 +0800

    使用pandoc来清理html，效果比hxclean和hxnormailize似乎更好

--- parse_url_by_manual.sh
+++ parse_url_by_manual.sh
@@ -10,24 +10,32 @@ title_selector=$(echo ${parse_cfg}|jq -r ".title")
 if [[ -z "${title_selector}" ]];then
     title_selector=".title"
 fi
-title=$(echo ${html}|hxclean|hxselect -c "${title_selector}"|pandoc -f html -t plain)
+if [[ -n "${title_selector}" ]];then
+    title=$(echo ${html}|pandoc -f html -t html -s|hxselect -c "${title_selector}"|pandoc -f html -t plain)
+fi
+
 # extract author
 author_selector=$(echo ${parse_cfg}|jq -r ".author")
-author=$(echo ${html}|hxclean|hxselect -c "${author_selector}"|pandoc -f html -t plain)
+if [[ -n "${author_selector}" ]];then
+    author=$(echo ${html}|pandoc -f html -t html -s|hxselect -c "${author_selector}"|pandoc -f html -t plain)
+fi
+
 # extract date
 date_selector=$(echo ${parse_cfg}|jq -r ".date")
-date=$(echo ${html}|hxclean|hxselect -c "${date_selector}"|pandoc -f html -t plain)
-date=$(date -d "${date}" "+%Y%m%d") # 格式化date
+if [[ -n "${date_selector}" ]];then
+    date=$(echo ${html}|pandoc -f html -t html -s|hxselect -c "${date_selector}"|pandoc -f html -t plain)
+    date=$(date -d "${date}" "+%Y%m%d") # 格式化date
+fi
 # extract content
 while read content_selector
 do
-    content_part=$(echo ${html}|hxclean|hxselect "${content_selector}")
+    content_part=$(echo ${html}|pandoc -f html -t html -s|hxselect "${content_selector}")
     content="${content}
 ${content_part}"
 done< <(echo ${parse_cfg}|jq -r ".content[]")
 while read exclude_selector
 do
-    content=$(echo ${content}|hxclean|hxremove -i "${exclude_selector}")
+    content=$(echo ${content}|pandoc -f html -t html -s|hxremove -i "${exclude_selector}")
 done< <(echo ${parse_cfg}|jq -r ".exclude[]")
 echo '{}'|jq '{"title":$title,
                 "author":$author,

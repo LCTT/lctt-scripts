02b2d0404c85f083e18bbcfb186e61f6af0a0119
darksun <lujun9972@gmail.com>
2019-04-09 17:57:33 +0800

    update

--- auto_add.sh
+++ auto_add.sh
@@ -1,10 +1,15 @@
 #!/bin/env bash
 # feeds = ("http://feeds.feedburner.com/Ostechnix", "https://www.2daygeek.com/feed/", "https://fedoramagazine.org/feed/", "https://www.networkworld.com/index.rss", "https://feeds.feedburner.com/kerneltalks", "https://www.linux.com/feeds/blogs/community/rss", "https://www.linuxtechi.com/feed/", "https://www.datamation.com/rss.xml", "http://lukasz.langa.pl/feed/recent/rss-en.xml", "https://itsfoss.com/feed/", "https://feeds.feedburner.com/LinuxUprising", "https://linuxaria.com/feed", "https://dave.cheney.net/feed")
-# feeds = ("https://www.2daygeek.com/feed/", "https://fedoramagazine.org/feed/", "https://www.networkworld.com/index.rss", "https://www.linux.com/feeds/blogs/community/rss", "https://itsfoss.com/feed/", "https://opensource.com/feed")
-feed="$1"
-./feed_monitor.py "${feed}" |while read url
-do
-yes "
+# "https://www.2daygeek.com/feed/"
+feeds="https://fedoramagazine.org/feed/ https://www.networkworld.com/index.rss https://www.linux.com/feeds/blogs/community/rss https://itsfoss.com/feed/ https://opensource.com/feed"
+if [[ $# -ne 0 ]];then
+    feeds="$*"
+fi
+for feed in "${feeds}";do
+    ./feed_monitor.py "${feed}" |while read url
+    do
+        yes "
 "|./1_add_new_article_manual.sh -u "${url}" -c tech
-./4_finish.sh -d
+        ./4_finish.sh -d
+    done
 done

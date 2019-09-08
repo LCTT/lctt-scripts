9ea955fefe6500ddc1d763561e602d81b9a12346
darksun <lujun9972@gmail.com>
2019-09-08 20:57:53 +0800

    update

--- auto_translate.sh
+++ auto_translate.sh
@@ -2,37 +2,63 @@
 set -e
 source base.sh
 
-article="$*"
-position="元数据"            # 其他可能值包括 "正文","引用","结尾"
-while read line
-do
-    echo "${line}"
-    if [[ "${line}" != *[a-zA-Z]* ]];then
-        continue                # 至少包含一个英文字母才需要翻译
-    fi
-    if [[ "${position}" == "元数据" ]];then
-        if [[ "${line}" == \[#\]:* ]];then
-            continue
-        else
-            position="正文"
-        fi
-    fi
+while getopts :rf: OPT; do
+    case $OPT in
+        r|+r)
+            replace_flag="True"
+            ;;
+        f|+f)
+            source_file="$OPTARG"
+            ;;
+        *)
+            echo "usage: ${0##*/} [+-rf ARG} [--] ARGS..."
+            exit 2
+    esac
+done
+shift $(( OPTIND - 1 ))
+OPTIND=1
 
-    if [[ "${line}" == '```' ]];then
-        if [[ "${position}" != "引用" ]];then
-            position="引用"
-            continue
-        else
-            position="正文"
-        fi
-    fi
-    
-    if [[ "${line}" == "--------------------------------------------------------------------------------" ]];then
-        position="结尾"
-        break
-    fi
+if [[ "${replace_flag}" == "True" ]];then
+    dest_file="${source_file}"
+else
+    dest_file="/dev/tty"
+fi
 
-    if [[ "${position}" == "正文" ]];then
-        youdao.sh "${line}"
-    fi
-done < <(cat "${article}")
+function do_translate()
+{
+    article="$*"
+    position="元数据"            # 其他可能值包括 "正文","引用","结尾"
+    while read line
+    do
+        echo "${line}"
+        if [[ "${position}" == "元数据" ]];then
+            if [[ "${line}" == \[#\]:* ]];then
+                continue
+            else
+                position="正文"
+            fi
+        fi
+
+        if [[ "${line}" == '```'* ]];then
+            if [[ "${position}" != "引用" ]];then
+                position="引用"
+                echo 引用----------------------------------------------------
+                continue
+            else
+                position="正文"
+            fi
+        fi
+
+        if [[ "${line}" == "--------------------------------------------------------------------------------"* ]];then
+            position="结尾"
+            continue
+        fi
+
+        if [[ "${position}" == "正文" && "${line}" == *[a-zA-Z]*  ]];then
+            youdao.sh "${line}" # 至少包含一个英文字母才需要翻译
+        fi
+    done < <(cat "${article}")
+}
+
+translated_content="$(do_translate "${source_file}")"
+echo "${translated_content}" > "${dest_file}"
